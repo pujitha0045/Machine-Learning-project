de82fa7333951ed6d99098b23231c3c7695889f4
Pujitha Gandipadala <167332068+pujitha0045@users.noreply.github.com>
2024-04-17 22:08:53 +0530

    Update rvsm.py

--- rvsm.py
+++ rvsm.py
@@ -1,6 +1,6 @@
 # Machine-Learning-project
-Rock vs Mine under water
-Importing the Dependencies
+#Rock vs Mine under water
+#Importing the Dependencies
 
 import numpy as np
 import pandas as pd
@@ -30,7 +30,7 @@ Y = sonar_data[60]
 print(X)
 print(Y)
 
-Training and Test data
+#Training and Test data
 
  
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.1, stratify=Y, random_state=1)
@@ -38,9 +38,7 @@ print(X.shape, X_train.shape, X_test.shape)
 print(X_train)
 print(Y_train)
 
-Model Training --> Logistic Regression
-
- 
+#Model Training --> Logistic Regression
 model = LogisticRegression()
 
 #training the Logistic Regression model with training data
@@ -59,7 +57,7 @@ test_data_accuracy = accuracy_score(X_test_prediction, Y_test)
 
 print('Accuracy on test data : ', test_data_accuracy)
 
-Making a Predictive System
+#Making a Predictive System
 
 input_data = (0.0307,0.0523,0.0653,0.0521,0.0611,0.0577,0.0665,0.0664,0.1460,0.2792,0.3877,0.4992,0.4981,0.4972,0.5607,0.7339,0.8230,0.9173,0.9975,0.9911,0.8240,0.6498,0.5980,0.4862,0.3150,0.1543,0.0989,0.0284,0.1008,0.2636,0.2694,0.2930,0.2925,0.3998,0.3660,0.3172,0.4609,0.4374,0.1820,0.3376,0.6202,0.4448,0.1863,0.1420,0.0589,0.0576,0.0672,0.0269,0.0245,0.0190,0.0063,0.0321,0.0189,0.0137,0.0277,0.0152,0.0052,0.0121,0.0124,0.0055)
 
